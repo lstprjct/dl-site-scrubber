cd820907e175ccc42d0eb1fd0394aeebe83ef0dc
PrimePlaya24 <62297998+PrimePlaya24@users.noreply.github.com>
2020-03-17 11:07:37 -0500

    Add files via upload

---
+++ scripts/SiteScrubber-UploadRAR.meta.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         SiteScrubber - UploadRAR
+// @namespace    SiteScrubber
+// @version      0.1
+// @description  Scrub site of ugliness and ease the process of downloading
+// @author       PrimePlaya24
+// @icon         https://raw.githubusercontent.com/PrimePlaya24/dl-site-scrubber/master/icons/uploadrar_favicon.png
+// @homepageURL  https://github.com/PrimePlaya24/dl-site-scrubber
+// @supportURL   https://github.com/PrimePlaya24/dl-site-scrubber/issues
+// @updateURL    https://raw.githubusercontent.com/PrimePlaya24/dl-site-scrubber/master/scripts/SiteScrubber-UploadRAR.meta.js
+// @downloadURL  https://raw.githubusercontent.com/PrimePlaya24/dl-site-scrubber/master/scripts/SiteScrubber-UploadRAR.user.js
+// @match        https://uploadrar.com/*
+// @require      https://code.jquery.com/jquery-1.9.1.min.js
+// @grant        none
+// ==/UserScript==
